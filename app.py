d7ae832a189c1cd282e742d29cd28bd598a1494f
Michael Legg <mlegg@udel.edu>
2020-08-18 17:10:45 -0400

    Debugging

--- app.py
+++ app.py
@@ -41,6 +41,7 @@ def home():
 def precipitation():
 #    """ Convert the query results to a dictionary using date as the key and prcp as the value. Return the JSON representation of your dictionary."""
     lastdate = session.query(func.max(measurement.date)).all()[0][0]
+    session.close()
     lastdate = dt.datetime.strptime(lastdate, '%Y-%m-%d')
     year_ago_date = lastdate - dt.timedelta(365)
     year_data = session.query(measurement.date, measurement.prcp)\
@@ -58,6 +59,7 @@ def precipitation():
 def stations():
     # Return a JSON list of stations from the dataset.
     stationsQuery = session.query(station.name).all()
+    session.close()
     stationData = []
     for station_ in stationsQuery[:1]:
         stations_Dict = {"Name": stationsQuery}
@@ -69,11 +71,13 @@ def stations():
 def tobs():
     # Query the dates and temperature observations of the most active station for the last year of data. Return a JSON list of temperature observations (TOBS) for the previous year.
     lastdate = session.query(func.max(measurement.date)).all()[0][0]
+    session.close()
     lastdate = dt.datetime.strptime(lastdate, '%Y-%m-%d')
     year_ago_date = lastdate - dt.timedelta(365)
     most_active_station_data = (session.query(measurement.date, measurement.station, measurement.tobs)\
                                 .filter(measurement.date >= year_ago_date)\
                                 .order_by(measurement.date.desc()).all())
+    session.close()
     tobsList = []
     for data in most_active_station_data:
         tobsDict = {"Station": data.station, data.date: data.tobs}
@@ -84,9 +88,13 @@ def tobs():
 @app.route("/api/v1.0/<start>")
 def start(start):
     low = (session.query(func.min(measurement.tobs)).filter(measurement.date >= start)).all()[0][0]
+    session.close()
     high = (session.query(func.max(measurement.tobs)).filter(measurement.date >= start)).all()[0][0]
+    session.close()
     avg = (session.query(func.avg(measurement.tobs)).filter(measurement.date >= start)).all()[0][0]
+    session.close()
     avg = round(avg, 2)
+
 
     return jsonify(low, high, avg)
 
@@ -95,10 +103,16 @@ def start(start):
 @app.route("/api/v1.0/<start>/<end>")
 def startEnd(start, end):
     low = (session.query(func.min(measurement.tobs)).filter(measurement.date >= start).filter(measurement.date <= end)).all()[0][0]
+    session.close()
     high = (session.query(func.max(measurement.tobs)).filter(measurement.date >= start).filter(measurement.date <= end)).all()[0][0]
+    session.close()
     avg = (session.query(func.avg(measurement.tobs)).filter(measurement.date >= start).filter(measurement.date <= end)).all()[0][0]
+    session.close()
     avg = round(avg, 2)
 
     return jsonify(low, high, avg)
+
+
+    
 if __name__ == "__main__":
     app.run(debug=True)
